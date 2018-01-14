6f8ebaf6bc522b2de7e35c97845ccc9433183c1a
Michael <mkural@mit.edu>
2018-01-13 21:43:28 -0500

    fix concave shapes

--- static/game.js
+++ static/game.js
@@ -2,27 +2,86 @@ var globals = {
     score: 0,
     dimensions: {x: 200, y: 200, },
 };
+var add = function(first_point, second_point){
+    return {x:first_point.x+second_point.x,y:first_point.y+second_point.y,};
+}
+var signed_twice_area = function(points){
 
-var gen_convex_polygon = function(dimensions) {
-    var num_points = 5;
-    var edges =[];
-    var sum_vec = {x:0,y:0,};
+    var ans = 0;
+    for(var i = 0;i<points.length;i++){
+        ans+=points[i].x*points[(i+1)%points.length].y;
+        ans-=points[i].y*points[(i+1)%points.length].x;
+    }
+    return ans;
+}
+
+var gen_sides = function(num_points){
+    var edges = [];
+    var edge_sum = {x:0,y:0,};
     for(var i = 0;i<num_points;i++){
         edges.push({x:Math.random(),y:Math.random()});
-        sum_vec.x+=edges[edges.length-1].x;
-        sum_vec.y+=edges[edges.length-1].y;
+        edge_sum = add(edge_sum,edges[edges.length-1]);
     }
-    sum_vec.x/=num_points;
-    sum_vec.y/=num_points;
-
+    edge_sum.x/=num_points;
+    edge_sum.y/=num_points;
     for(var i = 0;i<num_points;i++){
-        edges[i].x-=sum_vec.x;
-        edges[i].y-=sum_vec.y;
+        edges[i].x-=edge_sum.x;
+        edges[i].y-=edge_sum.y;
     }
-    edges.sort(function(pt1,pt2){
-        return Math.atan2(pt1.x,pt1.y)-Math.atan2(pt2.x,pt2.y);
-    });
+    return edges;
+}
+var valid_polygon = function(edges){
+    var pos = [{x:0,y:0,}];
+    for(var i = 0;i<edges.length-1;i++){
+        pos.push(add(pos[i],edges[i]));
+    }
+    //pos[0] = (0,0)
+    //pos[1] = (0,0)+edges[0]
+    for(var i = 0;i<edges.length;i++){
+        for(var j = i+2;j<edges.length;j++){
+            if((Math.abs(i-j)%edges.length)==1) continue;
+            if(edge_intersect(pos[i],edges[i],pos[j],edges[j])){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+var edge_intersect = function(first_pos, first_edge, second_pos,second_edge){
+    var first_begin = first_pos;
+    var first_end = add(first_pos,first_edge);
+    var second_begin = second_pos;
+    var second_end = add(second_pos,second_edge);
+    if(signed_twice_area([first_begin,second_begin,second_end])*signed_twice_area([first_end,second_begin,second_end])>0) return false;
+    if(signed_twice_area([second_begin,first_begin,first_end])*signed_twice_area([second_end,first_begin,first_end])>0) return false;
+    return true;
+}
+var gen_convex_polygon = function(dimensions, concave = false) {
+    // TODO
+    var num_points = 5;
+    var concave_tries = 1000;
+    if(concave){
+        while(concave_tries>0){
+            edges = gen_sides(num_points);
+            if(valid_polygon(edges)){
+                break;
+            }
+            concave_tries--;
+        }
+        if(concave_tries==0){
+            edges = gen_sides(num_points);
+            edges.sort(function(pt1,pt2){
+                return Math.atan2(pt1.x,pt1.y)-Math.atan2(pt2.x,pt2.y);
+            });  
+        }
+    }
+    else{
+        edges = gen_sides(num_points);
+        edges.sort(function(pt1,pt2){
+            return Math.atan2(pt1.x,pt1.y)-Math.atan2(pt2.x,pt2.y);
+        });    
 
+    }
     var points = [];
     var min_x = 0;
     var min_y = 0;
@@ -30,7 +89,7 @@ var gen_convex_polygon = function(dimensions) {
     var max_y = 0;
     points.push({x:0,y:0,});
     for(var i = 1;i<num_points;i++){
-        points.push( {x:points[i-1].x + edges[i].x, y:points[i-1].y + edges[i].y,});
+        points.push( {x:points[i-1].x + edges[i-1].x, y:points[i-1].y + edges[i-1].y,});
         min_x = Math.min(min_x, points[i].x);
         min_y = Math.min(min_y, points[i].y);
         max_x = Math.max(max_x, points[i].x);
@@ -40,18 +99,13 @@ var gen_convex_polygon = function(dimensions) {
         points[i].x = dimensions.x*(points[i].x-min_x)/(max_x-min_x);
         points[i].y = dimensions.y*(points[i].y-min_y)/(max_y-min_y);
     }
+
     return points;
 
 }
 
 var twice_area = function(points){
-    var ans = 0;
-    for(var i = 0;i<points.length;i++){
-        ans+=points[i].x*points[(i+1)%points.length].y;
-        ans-=points[i].y*points[(i+1)%points.length].x;
-    }
-    return Math.abs(ans);
-
+    return Math.abs(signed_twice_area(points));
 }
 var shape_html = function(dimensions, points) {
     var points_stringify = "'";
